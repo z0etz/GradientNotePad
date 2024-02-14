c3f0aadc1e740d60d65dcff8af5ceaae8dbd82c6
Katja <katja@klahr.se>
2024-02-14 11:18:17 +0100

    Initial Commit

---
+++ GradientNotePad/GradientNotePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientNotePadApp.swift
+//  GradientNotePad
+//
+//  Created by Katja Klahr on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct GradientNotePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
